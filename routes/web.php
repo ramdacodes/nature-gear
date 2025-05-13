793e6cc2e27fe0be17e7c633a483f6b1bdcb5641
Ferdy Rahmat Ramdani <ramda.codes@gmail.com>
2025-05-13 23:13:38 +0700

    feat: integrate Xendit PHP SDK and add test route for invoice creation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Xendit\Configuration;
+use Xendit\Invoice\InvoiceApi;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -13,5 +15,28 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/test-xendit', function () {
+    Configuration::setXenditKey(config('services.xendit.secret_key'));
+
+    $apiInstance = new InvoiceApi();
+    $create_invoice_request = new Xendit\Invoice\CreateInvoiceRequest([
+        'external_id' => 'test1234',
+        'description' => 'Test Invoice',
+        'amount' => 10000,
+        'invoice_duration' => 172800,
+        'currency' => 'IDR',
+        'reminder_time' => 1
+    ]);
+
+    try {
+        $invoice = $apiInstance->createInvoice($create_invoice_request);
+
+        return redirect($invoice['invoice_url']);
+    } catch (\Xendit\XenditSdkException $e) {
+        echo 'Exception when calling InvoiceApi->createInvoice: ', $e->getMessage(), PHP_EOL;
+        echo 'Full Error: ', json_encode($e->getFullError()), PHP_EOL;
+    }
+});
