b5b1f82f0824c88d7e449d6a86efadaff83ceb06
Ferdy Rahmat Ramdani <ramda.codes@gmail.com>
2025-05-15 19:16:31 +0700

    feat: create Customer model and migration, and update Rental model with relationships

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Rental extends Model
 {
     protected $guarded = [];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+
+    public function rentalDetail()
+    {
+        return $this->hasMany(RentalDetail::class, 'rental_id', 'id');
+    }
 }
